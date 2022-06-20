41eec05f49f7f3cead21e87b84b80c957d60c0a6
BlueZeeKing <github@blueish.dev>
2022-06-20 11:08:24 -0400

    Finish question selector

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircle, faCircleCheck } from "@fortawesome/free-regular-svg-icons";
+import { faCircle } from "@fortawesome/free-regular-svg-icons";
+import circle from "./circleIcon";
+import { motion } from "framer-motion"
 
 const answers = ["Answer 1", "Answer 2", "Answer 3"];
 
@@ -15,14 +17,21 @@ export default function Question() {
         <div className="py-4 flex flex-col">
           {answers.map((item, index) => (
             <button
-              className="text-left rounded bg-white hover:bg-zinc-100 active:bg-zinc-200 pr-4 py-2 my-1 transition duration-100"
+              className="text-left rounded bg-white hover:bg-zinc-100 active:bg-zinc-200 pr-4 py-2 my-1 transition duration-100 relative"
               key={item}
               onClick={() => setSelected(index)}
             >
-              <FontAwesomeIcon
-                className="px-2"
-                icon={selected == index ? faCircleCheck : faCircle}
-              />
+              <FontAwesomeIcon className="px-2" icon={faCircle} />
+              {selected == index ? (
+                <motion.div
+                  className="absolute top-3 left-3 z-40"
+                  layoutId="selector"
+                >
+                  <FontAwesomeIcon className="w-2 absolute z-50" icon={circle} />
+                </motion.div>
+              ) : (
+                ""
+              )}
               item
             </button>
           ))}
---
+++ src/components/circleIcon.tsx
@@ -0,0 +1,3 @@
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
+
+export default faCircle
